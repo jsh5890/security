91d0f6cd6e47e12b656496e978c3d8e1435d84af
DESKTOP-I41R9CA\jsh <jmao>
2021-01-04 23:44:23 +0900

    스프링부트 시큐리티 7강 - 구글 회원 프로필 정보 받아보기

--- src/main/java/com/jsh/security1/config/SecurityConfig.java
+++ src/main/java/com/jsh/security1/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.jsh.security1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,10 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.jsh.security1.config.oauth.PrincipalOauth2UserService;
+
 @Configuration
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록됩니다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // 시큐어 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService; 
 	
 	//해당 메서드의 리턴되는 오브젝트를 ioc로 등록한다.
 	@Bean
@@ -31,6 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.formLogin()
 			.loginPage("/loginForm")
 			.loginProcessingUrl("/login")// /login 호출되면 시큐리티가 낚아채서 대신 로그인 진행
-			.defaultSuccessUrl("/");
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm")//구글로그인 완료후 후처리 필요 1.코드받기 2. 엑세스토큰 3.사용자프로필정보4. 정보토대로 회원가입
+			.userInfoEndpoint()
+			.userService(principalOauth2UserService);
 	}
 }
